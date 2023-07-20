96ecf0f7c4659273fe3947fdeedc8755b7e198ed
Tulenishka <ili.vovan@yandex.ru>
2023-07-20 16:30:54 +0300

    Create LotteryRepository.java

---
+++ src/main/java/com/example/TechWearBot/model/LotteryTable/LotteryRepository.java
@@ -0,0 +1,19 @@
+package com.example.TechWearBot.model.LotteryTable;
+
+
+import com.example.TechWearBot.model.LotteryTable.Lottery;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface LotteryRepository extends CrudRepository <Lottery, Long>{
+
+      @Query(value = "select ticket from lottery where chat_id = :chatId", nativeQuery = true)
+    int getTicket(long chatId);
+
+    @Query(value = "select ticket from lottery where ticket = (select max(ticket) from lottery)", nativeQuery = true)
+    int getMaxTicket();
+
+}
+
+
