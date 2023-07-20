96c111368ec943dd2dbffce8e3780c51c3989602
Tulenishka <ili.vovan@yandex.ru>
2023-07-20 16:30:59 +0300

    Create TechWearBotApplication.java

---
+++ src/main/java/com/example/TechWearBot/TechWearBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.TechWearBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  TechWearBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechWearBotApplication.class, args);
+	}
+
+}
