1e315623c896b69d19406478d38f50e13cd89566
Tulenishka <ili.vovan@yandex.ru>
2023-07-20 16:31:07 +0300

    Create TechWearBotApplicationTests.java

---
+++ src/test/java/com/example/TechWearBot/TechWearBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TechWearBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechWearBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
