25fb1f4044078c592f82878722d41863274fc868
Tulenishka <ili.vovan@yandex.ru>
2023-07-20 16:30:51 +0300

    Create Lottery.java

---
+++ src/main/java/com/example/TechWearBot/model/LotteryTable/Lottery.java
@@ -0,0 +1,46 @@
+package com.example.TechWearBot.model.LotteryTable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Lottery {
+    @Id
+    private Long chatId;
+    private String userName;
+    private Integer ticket;
+    public Long getChatId() {
+        return chatId;
+    }
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+
+    }
+
+    public void setTicket(Integer ticket) {
+        this.ticket = ticket;
+    }
+
+    public Integer getTicket() {
+        return ticket;
+    }
+
+    @Override
+    public String toString() {
+        return "Lottery{" +
+                "chatId=" + chatId +
+                ", userName='" + userName + '\'' +
+                ", ticket=" + ticket +
+                '}';
+    }
+}
